a523ca057ccda9d38bb95022d23fdf7d078b29ee
mehdiboutoumit <mehdiboutoumit03@gmail.com>
2025-01-01 22:48:04 +0100

    Middle Man code smell

--- src/main/java/com/mehdirefactoring/com/thisisrefactoring/repository/CommentRepository.java
+++ src/main/java/com/mehdirefactoring/com/thisisrefactoring/repository/CommentRepository.java
@@ -1,23 +1,15 @@
 package com.mehdirefactoring.com.thisisrefactoring.repository;
 
+import com.mehdirefactoring.com.thisisrefactoring.model.BlogPost;
 import com.mehdirefactoring.com.thisisrefactoring.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class CommentRepository {
-    private final List<Comment> comments = new ArrayList<>();
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    public Comment save(Comment comment) {
-        comments.add(comment);
-        return comment;
-    }
+    public List<Comment> findByBlogPostId(Long blogPost);
 
-    public List<Comment> findByBlogPostId(Long blogPostId) {
-        return comments.stream()
-                .filter(comment -> comment.getBlogPostId().equals(blogPostId))
-                .toList();
-    }
 }
--- src/main/java/com/mehdirefactoring/com/thisisrefactoring/service/CommentService.java
+++ src/main/java/com/mehdirefactoring/com/thisisrefactoring/service/CommentService.java
@@ -2,19 +2,29 @@ package com.mehdirefactoring.com.thisisrefactoring.service;
 
 import com.mehdirefactoring.com.thisisrefactoring.model.Comment;
 import com.mehdirefactoring.com.thisisrefactoring.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+// Middle Man Code Smell
 @Service
 public class CommentService {
 
-    private final CommentRepository commentRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
-    public CommentService(CommentRepository commentRepository) {
-        this.commentRepository = commentRepository;
-    }
 
     public Comment addComment(Comment comment) {
-        // No validation for comment content
-        return commentRepository.save(comment);
+        return commentRepository.save(comment); // Delegating to CommentRepository
+    }
+
+    public Comment getCommentById(Long commentId) {
+        return commentRepository.findById(commentId).orElse(null); // Delegating to CommentRepository
+    }
+
+    public List<Comment> getAllCommentsForPost(Long blogPostId) {
+        return commentRepository.findByBlogPostId(blogPostId); // Delegating to CommentRepository
     }
 }
+
